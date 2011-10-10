212a85a8f6a1fd4fa0fc1cbbe53683b34519a489
root <root@imakadmin-OptiPlex-755>
2011-10-10 14:48:53 -0500

    First Commit

---
+++ BasicMathProject/src/BasicMathProject.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class BasicMathProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanme = new Scanner(System.in);
+		
+		System.out.println("Enter Your First Number");
+		
+		System.out.println("Enter Your Second Number");
+
+		System.out.println("Press 1 to add, 2 to subtract, 3 to multiply, 4 to divide");
+		 
+	}
+	
+	public static int addemup(int x, int y)
+	{
+		int z = x + y;
+		return z;
+	}
+	public static int subtractemdown(int x, int y)
+	{
+		int z = x - y;
+		return z;
+	}
+	public static int multiplymethod(int x, int y)
+	{
+		int z = x * y;
+		return z;
+	}
+	public static int dividetwonumbers(int x, int y)
+	{
+		int z = x / y;
+		return z;
+	}
+
+}
